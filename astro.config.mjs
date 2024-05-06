8f6383849652cc1849a4af37c432b89878731b34
Aryanmath7 <aryanmath7@gmail.com>
2024-05-06 16:58:10 -0500

    commit actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+
+
+
 // https://astro.build/config
-export default defineConfig({});
+// export default defineConfig({});
+
+export default defineConfig({
+    site: 'https://Aryanmath7.github.io',
+    base: 'Sean-C-Portfolio',
+  })
+
+
